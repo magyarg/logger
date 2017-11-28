8d575ce7b3c0c2db10483b54bcabe4690879aef7
magyarg <magyar.gabor1@gmail.com>
2017-11-28 11:53:21 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+### SX-GUI - Logger
+
+Simple logger package for any GUI application with log rotation.
+
+### Installation
+
+* Download the repository and copy the Logger.js into your project
+* Include into the scripts file and create a new instance with the ``` let logger = new Logger(); ``` line of code.
+
+### Usage
+
+* INFO level: ``` logger.info("Message") ```
+* DEBUG level: ``` logger.debug("Message") ```
+* WARNING level: ``` logger.warning("Message") ```
+* ERROR level: ``` logger.info("ERROR") ```
+
+### Changelog
+
+#### 0.0.1
+* Implemented interface for common helpers (DateTime)
+* Implemented info level logging
---
+++ src/Logger.js
@@ -0,0 +1,55 @@
+
+function Logger() {
+
+    /**
+     * Current log level
+     */
+    this.logLevel = '';
+
+    /**
+     * Current log message
+     */
+    this.logMessage
+
+    /**
+     * Returns an INFO level log entry
+     */
+    this.info = function (logMessage) {
+        this.logLevel = 'INFO';
+        this.logMessage = logMessage;
+        this.resolve();
+    }
+
+    /**
+     * Returns a DEBUG level log entry
+     */
+    this.debug = function (logMessage) {
+        this.logLevel = 'DEBUG';
+        this.logMessage = logMessage;
+        this.resolve();
+    }
+
+    /**
+     * Returns an WARNING level log entry
+     */
+    this.warning = function (logMessage) {
+        this.logLevel = 'WARNING';
+        this.logMessage = logMessage;
+        this.resolve();
+    }
+
+    this.resolve = function() {
+        console.log("[" + this.dateTime() + "][" + this.logLevel + "] - " + this.logMessage);
+    }
+
+    /**
+     * Returns a formatted string as
+     * the current date for any log entries
+     * @return String
+     */
+    this.dateTime = function() {
+        let currentDate = new Date();
+        return currentDate.getFullYear() + "/" + currentDate.getMonth() + "/" + currentDate.getDate() + " - " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+    }
+
+}
